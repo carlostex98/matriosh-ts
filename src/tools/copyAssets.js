a84ed82247233d6abd6729e4f9270f08472ad8bf
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-09-18 21:47:22 -0600

    Update

--- src/tools/copyAssets.js
+++ src/tools/copyAssets.js
@@ -5,3 +5,5 @@ var first = 123;
 // Copy all the view templates
 shell.cp("-R", "src/views", "build/");
 shell.cp("-R", "src/gram", "build/");
+shell.cp("-R", "src/Interpreter", "build/");
+shell.cp("-R", "src/ast", "build/");
