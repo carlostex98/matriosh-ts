448c60222c4bf682289d51f5e84121ac7697bb33
Carlos Alejandro Tenes Mejia <cctenes@gmail.com>
2020-09-19 21:07:17 -0600

    Env update
    Based on Erik Flores interpreter

---
+++ src/Symbol/Environment.ts
@@ -0,0 +1,77 @@
+import { env } from "process"
+import { Symbol } from "./Symbol";
+import { Type } from "../Abstract/ret_v";
+import { Function } from "../Instruction/Function";
+
+export class Environment{
+    
+    private variables : Map<string, Symbol>;
+    public funciones : Map<string, Function>;
+
+    constructor(public anterior : Environment | null){
+        this.variables = new Map();
+        this.funciones = new Map();
+    }
+
+    public guardar(id: string, valor: any, type: Type){
+        let env : Environment | null = this;
+        while(env != null){
+            if(env.variables.has(id)){
+                env.variables.set(id, new Symbol(valor, id, type));
+                return;
+            }
+            env = env.anterior;
+        }
+        this.variables.set(id, new Symbol(valor, id, type));
+    }
+
+    public guardarFuncion(id: string, funcion : Function){
+        //TODO ver si la funcion ya existe, reportar error
+        this.funciones.set(id, funcion);
+    }
+
+    public getVar(id: string) : Symbol | undefined | null{
+        let env : Environment | null = this;
+        while(env != null){
+            if(env.variables.has(id)){
+                return env.variables.get(id);
+            }
+            env = env.anterior;
+        }
+        return null;
+    }   
+
+    public getFuncion(id: string) : Function | undefined{
+        let env : Environment | null = this;
+        while(env != null){
+            if(env.funciones.has(id)){
+                return env.funciones.get(id);
+            }
+            env = env.anterior;
+        }
+        return undefined;
+    }
+
+    public getGlobal() : Environment{
+        let env : Environment | null = this;
+        while(env?.anterior != null){
+            env = env.anterior;
+        }
+        return env;
+    }
+
+    public setVar(id: string, valor: any, type: Type){
+
+    }
+}
+/*envGlobal
+
+function X() {
+    env
+    env.anterior = envGlobal;
+    if(1){
+        envIf
+        envIf.anterior = env
+
+    }
+}*/ 
